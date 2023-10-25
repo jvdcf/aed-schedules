c5ae5c3b26c30f08fb56f20221a0ebb60ea7e683
Guilherme Matos <up202208755@up.pt>
2023-10-25 16:02:44 +0100

    changing std::sort to std::stable_sort

--- src/classes.hpp
+++ src/classes.hpp
@@ -1,75 +1,75 @@
 #include <cctype>
+#include <cstdint>
 #include <iostream>
 #include <string>
-#include <cstdint>
-
 
 #ifndef CLASS_HPP
 #define CLASS_HPP
 
 enum class WeekDay {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
 };
 
-enum class Type{
-    T,
-    TP,
-    PL,
+enum class Type {
+  T,
+  TP,
+  PL,
 
 };
 
-class Class{
+class Class {
 private:
-    uint16_t uc_code;
-    uint16_t class_code;
-    constexpr const static char* types_of_uc[] = {"L.EIC", "UP", "IAPD", "CSR", "IADE", "IR", "MPSAC", "DDS", "SEESTE"};
-    WeekDay day;
-    float start_hour;
-    float duration;
-    Type type;
+  uint16_t uc_code;
+  uint16_t class_code;
+  constexpr const static char *types_of_uc[] = {
+      "L.EIC", "UP", "IAPD", "CSR", "IADE", "IR", "MPSAC", "DDS", "SEESTE"};
+  WeekDay day;
+  float start_hour;
+  float duration;
+  Type type;
 
 public:
-    Class(std::string line); // Constructor
+  Class(std::string line); // Constructor
 
-    // Getters:
-    uint16_t getUcCode() const;
+  // Getters:
+  uint16_t getUcCode() const;
 
-    uint16_t getClassCode() const;
+  uint16_t getClassCode() const;
 
-    WeekDay getDay() const;
+  WeekDay getDay() const;
 
-    double getStartHour() const;
+  double getStartHour() const;
 
-    double getDuration() const;
+  double getDuration() const;
 
-    Type getType() const;
+  Type getType() const;
 
-    // Parsers:
-    uint16_t parse_uc(std::string uc_code);
+  // Parsers:
+  uint16_t parse_uc(std::string uc_code);
 
-    uint16_t parse_class(std::string class_code);
+  uint16_t parse_class(std::string class_code);
 
-    WeekDay parse_day(std::string day);
+  WeekDay parse_day(std::string day);
 
-    Type parse_type(std::string type);
+  Type parse_type(std::string type);
 
-    // To String:
-    void uc_to_str(std::string & out);
+  // To String:
+  void uc_to_str(std::string &out);
 
-    void class_to_str(std::string & out);
+  void class_to_str(std::string &out);
 
-    void day_to_str(std::string & out);
+  void day_to_str(std::string &out);
 
-    void type_to_str(std::string & out);
+  void type_to_str(std::string &out);
 
-    // Other Methods:
-    void display();
+  // Other Methods:
+  void display();
 };
 
 #endif // CLASS_HPP
