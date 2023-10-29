d96027f48be4f2fb2d2a3eeb67eed186561a82b0
João Vítor Ferreira <up202208393@up.pt>
2023-10-29 17:32:01 +0000

    main.cpp fixed

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
-<<<<<<< HEAD
-#include "src/CSVClasses.hpp"
-#include "src/CSVClassesPerUC.hpp"
-#include "src/CSVStudentsClasses.hpp"
-#include "src/Lesson.hpp"
-#include "src/ClassesPerUC.hpp"
-#include "src/StudentsClasses.hpp"
-=======
 /*
  * @file main.cpp
  * @brief Point of entry of the program.
  */
-
-#include "src/appClasses.hpp"
-#include "src/appClassesPerUC.hpp"
-#include "src/appStudentsClasses.hpp"
-#include "src/classes.hpp"
-#include "src/classesPerUC.hpp"
-#include "src/studentsClasses.hpp"
->>>>>>> main
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include "src/CSVClasses.hpp"
+#include "src/CSVClassesPerUC.hpp"
+#include "src/CSVStudentsClasses.hpp"
+#include "src/ClassesPerUC.hpp"
+#include "src/StudentsClasses.hpp"
+#include "src/Lesson.hpp"
+#include "src/ClassSchedule.hpp"
+#include "src/Student.hpp"
+#include "src/Runtime.hpp"
+#include "src/Utils.hpp"
 
 int main(int argc, char** argv) {
-  AppClassPerUC cpu = AppClassPerUC("schedule/classes_per_uc.csv");
+  CSVClassPerUC cpu("schedule/classes_per_uc.csv");
   cpu.display();
 
-  AppStudentsClasses sc("schedule/students_classes.csv");
+  CSVStudentsClasses sc("schedule/students_classes.csv");
   sc.display();
 
-  AppClass ac("schedule/classes.csv");
+  CSVClasses ac("schedule/classes.csv");
   ac.display();
 
   return 0;
